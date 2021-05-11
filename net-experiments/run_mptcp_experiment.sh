38920e2ec2d80a8f6d1da29b8537ee08e0e7b135
Richard Sailer <richard_siegfried@systemli.org>
2021-05-11 19:59:02 +0200

    Add prototype: run_mptcp_experiment.sh

---
+++ net-experiments/run_mptcp_experiment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This runs a experiment with mptcp. And generates some graphs from it
+# (packet delay variation and throughput). This is the server side (T_exit)
+
+results_dir="MPTCP_in_tree_v5.12"   # the dir the results will be stored in
+echo -e "\e[31;1m$results_dir \e[0m"
+mkdir $results_dir
+
+# other_ctx_prefix="ip netns exec T_entry"
+tentry_ssh_dest="root@tentry"
+other_ctx_prefix="ssh $tentry_ssh_dest"
+
+# Restart MPTCP server (openvpn via MPTCP)
+# systemctl restart openvpn-server_mptcp@server.service
+
+# Restart MPTCP client (openvpn via MPTCP) to reinitialize tcp subflows
+$other_ctx_prefix  systemctl restart openvpn-client_mptcp@client
+sleep 2s
+
+# Run iperf client to generate test traffic
+$other_ctx_prefix "$probe_cmd $udp_flag  -t $runtime $bandwith_opt  \
+		            -c 192.168.65.2 -i $iperf_report_interval -e -f m  -P $flowcount  &> /tmp/iperf_tentry.log" &
+
+sleep $((runtime+13))
+## ----- here we block for 13 seconds, after that
+
+
+# Extract totals from iperf_tentry.log:
+# grep -v "^\[SUM\]" iperf_tentry.log | grep -e " 0\.0000-[1-9][0-9]" | grep -e "ms .* ms" -e "out-of-order" > iperf_totals
+# ./iperf_udp_report_aggregate.pl iperf_tentry.log > udp_aggregates.tsv
+
+mv  iperf_tentry.log   $results_dir
+mv $results_dir $series_dir
+
+sleep 4s
