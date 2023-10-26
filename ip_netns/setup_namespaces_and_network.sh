2735ff5fde2e1745ad125feb9be82d30004248f3
Richard Sailer <richard_siegfried@systemli.org>
2023-10-26 22:50:35 +0200

    setup_namespaces_and_network.sh avoid long lines

--- ip_netns/setup_namespaces_and_network.sh
+++ ip_netns/setup_namespaces_and_network.sh
@@ -24,25 +24,31 @@ ip link set veth32 netns T_exit
 # Configure the interfaces in T_entry
 ip netns exec T_entry ip addr add 1.0.0.1/24 dev veth11
 ip netns exec T_entry ip link set veth11 up
-tc -netns T_entry qdisc add dev veth11 root handle 20: netem delay 20ms rate 16mbit
+tc -netns T_entry qdisc add dev veth11 root handle 20: netem  \
+   delay 20ms rate 16mbit
 
 ip netns exec T_entry ip addr add 2.0.0.1/24 dev veth21
 ip netns exec T_entry ip link set veth21 up
-tc -netns T_entry qdisc add dev veth21 root handle 20: netem delay 25ms rate 16mbit
+tc -netns T_entry qdisc add dev veth21 root handle 20: netem  \
+   delay 25ms rate 16mbit
 
 ip netns exec T_entry ip addr add 3.0.0.1/24 dev veth31
 ip netns exec T_entry ip link set veth31 up
-tc -netns T_entry qdisc add dev veth31 root handle 20: netem delay 25ms rate 32mbit
+tc -netns T_entry qdisc add dev veth31 root handle 20: netem  \
+   delay 25ms rate 32mbit
 
 # Configure the interfaces in T_exit
 ip netns exec T_exit ip addr add 1.0.0.2/24 dev veth12
 ip netns exec T_exit ip link set veth12 up
-tc -netns T_exit qdisc add dev veth12 root handle 20: netem delay 20ms rate 16mbit
+tc -netns T_exit qdisc add dev veth12 root handle 20: netem  \
+   delay 20ms rate 16mbit
 
 ip netns exec T_exit ip addr add 2.0.0.2/24 dev veth22
 ip netns exec T_exit ip link set veth22 up
-tc -netns T_exit qdisc add dev veth22 root handle 20: netem delay 25ms rate 16mbit
+tc -netns T_exit qdisc add dev veth22 root handle 20: netem  \
+   delay 25ms rate 16mbit
 
 ip netns exec T_exit ip addr add 3.0.0.2/24 dev veth32
 ip netns exec T_exit ip link set veth32 up
-tc -netns T_exit qdisc add dev veth32 root handle 20: netem delay 25ms rate 32mbit
+tc -netns T_exit qdisc add dev veth32 root handle 20: netem  \
+   delay 25ms rate 32mbit
